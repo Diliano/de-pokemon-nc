c5cb5857b6fc8b42dfd8b21544b694720781721b
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-09-19 15:22:12 +0100

    add functionality and passing test for constructing with a fire type

--- src/fire_pokemon.py
+++ src/fire_pokemon.py
@@ -1,4 +1,10 @@
 from src.pokemon import Pokemon
 
 class FirePokemon(Pokemon):
-    pass
+    def __init__(self, name, hit_points, attack_damage, move):
+        super().__init__(name, hit_points, attack_damage, move)
+        self.__type = "fire"
+
+    @property
+    def type(self):
+        return self.__type
--- test/test_fire_pokemon.py
+++ test/test_fire_pokemon.py
@@ -9,3 +9,7 @@ class TestInstantiation:
     def test_fire_pokemon_is_child_of_pokemon_class(self):
         my_pokemon = FirePokemon("Charmander", 44, 17, "Flamethrower")
         assert issubclass(type(my_pokemon), Pokemon)
+
+    def test_fire_pokemon_is_instantiated_with_fire_type(self):
+        my_pokemon = FirePokemon("Charmander", 44, 17, "Flamethrower")
+        assert my_pokemon.type == "fire"
