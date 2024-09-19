e7867b59ab46ee6beb301b7b7b0b469b7b98c86f
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-09-19 16:54:14 +0100

    Refactored Pokemon and FirePokemon classes, moving type, strong_against and weak_against properties into the Pokemon base class, as they will appear in all four subclasses

--- src/fire_pokemon.py
+++ src/fire_pokemon.py
@@ -3,18 +3,6 @@ from src.pokemon import Pokemon
 class FirePokemon(Pokemon):
     def __init__(self, name, hit_points, attack_damage, move):
         super().__init__(name, hit_points, attack_damage, move)
-        self.__type = "fire"
-        self.__strong_against = ["grass"]
-        self.__weak_against = ["water"]
-
-    @property
-    def type(self):
-        return self.__type
-    
-    @property
-    def strong_against(self):
-        return self.__strong_against
-    
-    @property
-    def weak_against(self):
-        return self.__weak_against
+        self._type = "fire"
+        self._strong_against = ["grass"]
+        self._weak_against = ["water"]
--- src/pokemon.py
+++ src/pokemon.py
@@ -4,6 +4,9 @@ class Pokemon:
         self._hit_points = hit_points
         self._attack_damage = attack_damage
         self._move = move
+        self._type = None
+        self._strong_against = []
+        self._weak_against = []
 
     @property
     def name(self):
@@ -20,6 +23,18 @@ class Pokemon:
     @property
     def move(self):
         return self._move
+    
+    @property
+    def type(self):
+        return self._type
+    
+    @property
+    def strong_against(self):
+        return self._strong_against
+    
+    @property
+    def weak_against(self):
+        return self._weak_against
     
     def use_move(self):
         return f"{self._name} used {self._move}!"
