0594c64b0537af785fbaef625fb7475d8a938120
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-09-19 16:47:05 +0100

    Refactored Pokemon base class to use non pub attributes, rather than private

--- src/pokemon.py
+++ src/pokemon.py
@@ -1,37 +1,37 @@
 class Pokemon:
     def __init__(self, name, hit_points, attack_damage, move):
-        self.__name = name
-        self.__hit_points = hit_points
-        self.__attack_damage = attack_damage
-        self.__move = move
+        self._name = name
+        self._hit_points = hit_points
+        self._attack_damage = attack_damage
+        self._move = move
 
     @property
     def name(self):
-        return self.__name
+        return self._name
     
     @property
     def hit_points(self):
-        return self.__hit_points
+        return self._hit_points
     
     @property 
     def attack_damage(self):
-        return self.__attack_damage
+        return self._attack_damage
     
     @property
     def move(self):
-        return self.__move
+        return self._move
     
     def use_move(self):
-        return f"{self.__name} used {self.__move}!"
+        return f"{self._name} used {self._move}!"
     
     def take_damage(self, damage):
         if damage < 0:
             return "Damage cannot be negative!"
         
-        if damage > self.__hit_points:
-            self.__hit_points = 0
+        if damage > self._hit_points:
+            self._hit_points = 0
         else:
-            self.__hit_points -= damage
+            self._hit_points -= damage
 
     def has_fainted(self):
-        return self.__hit_points == 0
+        return self._hit_points == 0
