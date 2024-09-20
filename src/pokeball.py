4850d2ef58a35a1c01540819320f6f2b37b731ee
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-09-20 14:18:18 +0100

    add functionality and passing tests for the catch() method of the Pokeball class

--- src/pokeball.py
+++ src/pokeball.py
@@ -1,3 +1,8 @@
+class PokeballFullError(Exception):
+    def __init__(self):
+        self.message = "The Pokeball already contains a Pokemon"
+        super().__init__(self.message)
+
 class Pokeball:
     def __init__(self):
         self._pokemon = None
@@ -5,3 +10,9 @@ class Pokeball:
     @property
     def pokemon(self):
         return self._pokemon
+    
+    def catch(self, pokemon):
+        if self._pokemon is None:
+            self._pokemon = pokemon
+        else:
+            raise PokeballFullError
--- test/test_pokeball.py
+++ test/test_pokeball.py
@@ -1,9 +1,19 @@
-from src.pokeball import Pokeball
+from src.pokeball import Pokeball, PokeballFullError
+from src.fire_pokemon import FirePokemon
+from src.water_pokemon import WaterPokemon
 import pytest
 
 @pytest.fixture
 def pokeball():
     return Pokeball()
+
+@pytest.fixture
+def fire_pokemon():
+    return FirePokemon("Charmander", 44, 17, "Flamethrower")
+
+@pytest.fixture
+def water_pokemon():
+    return WaterPokemon("Squirtle", 44, 16, "Surf")
 
 class TestInstantiation:
     def test_pokeball_instance_is_instance_of_pokeball_class(self, pokeball):
@@ -11,3 +21,14 @@ class TestInstantiation:
 
     def test_pokeball_is_instantiated_with_pokemon_property_none(self, pokeball):
         assert pokeball.pokemon == None
+
+class TestCatchMethod:
+    def test_catch_method_captures_and_stores_given_pokemon_if_pokeball_is_empty(self, pokeball, fire_pokemon):
+        pokeball.catch(fire_pokemon)
+        assert pokeball.pokemon == fire_pokemon
+
+    def test_catch_method_raises_pokeball_full_exception_if_pokeball_has_pokemon_already(self, pokeball, fire_pokemon, water_pokemon):
+        pokeball.catch(fire_pokemon)
+        with pytest.raises(PokeballFullError) as excinfo:
+            pokeball.catch(water_pokemon)
+        assert str(excinfo.value) == "The Pokeball already contains a Pokemon"
