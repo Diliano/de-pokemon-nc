e09297867d1f269ea58e5b476afed8b106f3ec6b
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-09-21 10:51:39 +0100

    add functionality and passing tests for the take_turn() method of the Battle class

--- src/battle.py
+++ src/battle.py
@@ -1,7 +1,13 @@
+class AlreadyFaintedError(Exception):
+    def __init__(self):
+        self.message = "A Pokemon has already fainted - time to rest!"
+        super().__init__(self.message)
+
 class Battle:
     def __init__(self, pokemon_1, pokemon_2):
         self._pokemon_1 = pokemon_1
         self._pokemon_2 = pokemon_2
+        self._pokemon_1_turn = True
 
     @property
     def pokemon_1(self):
@@ -10,3 +16,18 @@ class Battle:
     @property
     def pokemon_2(self):
         return self._pokemon_2
+    
+    @property
+    def pokemon_1_turn(self):
+        return self._pokemon_1_turn
+    
+    def take_turn(self):
+        if self._pokemon_1.has_fainted() or self._pokemon_2.has_fainted():
+            raise AlreadyFaintedError
+        if self._pokemon_1_turn:
+            damage = int(self._pokemon_1.attack_damage * self._pokemon_1.get_multiplier(self._pokemon_2))
+            self._pokemon_2.take_damage(damage)
+        else:
+            damage = int(self._pokemon_2.attack_damage * self._pokemon_2.get_multiplier(self._pokemon_1))
+            self._pokemon_1.take_damage(damage)
+        self._pokemon_1_turn = not self._pokemon_1_turn
--- test/test_battle.py
+++ test/test_battle.py
@@ -1,19 +1,61 @@
-from src.battle import Battle
+from src.battle import Battle, AlreadyFaintedError
 from src.fire_pokemon import FirePokemon
 from src.water_pokemon import WaterPokemon
 import pytest
 
 @pytest.fixture
-def fire_pokemon():
+def charmander():
     return FirePokemon("Charmander", 44, 17, "Flamethrower")
 
 @pytest.fixture
-def water_pokemon():
+def squirtle():
     return WaterPokemon("Squirtle", 44, 16, "Surf")
 
 class TestInstantiation:
-    def test_instance_is_instantiated_with_two_given_pokemon(self, fire_pokemon, water_pokemon):
-        battle = Battle(fire_pokemon, water_pokemon)
-        assert battle.pokemon_1 == fire_pokemon
-        assert battle.pokemon_2 == water_pokemon
+    def test_instance_is_instantiated_with_two_given_pokemon(self, charmander, squirtle):
+        battle = Battle(charmander, squirtle)
+        assert battle.pokemon_1 == charmander
+        assert battle.pokemon_2 == squirtle
 
+    def test_instance_is_instantiated_with_pokemon_1_turn_property(self, charmander, squirtle):
+        battle = Battle(charmander, squirtle)
+        assert battle.pokemon_1_turn is True
+
+class TestTakeTurnMethod:
+    def test_take_turn_method_starts_with_pokemon_1(self, charmander, squirtle):
+        battle = Battle(charmander, squirtle)
+        assert battle.pokemon_1_turn is True
+
+    def test_take_turn_method_calculates_and_deals_damage(self, charmander, squirtle):
+        battle = Battle(charmander, squirtle)
+        battle.take_turn()
+        assert squirtle.hit_points == 36
+
+    def test_take_turn_method_switches_attacker_after_each_call(self, charmander, squirtle):
+        battle = Battle(charmander, squirtle)
+        assert battle.pokemon_1_turn is True
+        battle.take_turn()
+        assert battle.pokemon_1_turn is False
+
+    def test_take_turn_method_handles_multiple_turns(self, charmander, squirtle):
+        battle = Battle(charmander, squirtle)
+        battle.take_turn()
+        assert squirtle.hit_points == 36
+        battle.take_turn()
+        assert charmander.hit_points == 20
+        battle.take_turn()
+        assert squirtle.hit_points == 28
+
+    def test_take_turn_method_raises_already_fainted_exception_if_a_pokemon_has_already_fainted(self, charmander, squirtle):
+        battle = Battle(charmander, squirtle)
+        battle.take_turn()
+        assert squirtle.hit_points == 36
+        battle.take_turn()
+        assert charmander.hit_points == 20
+        battle.take_turn()
+        assert squirtle.hit_points == 28
+        battle.take_turn()
+        assert charmander.hit_points == 0
+        with pytest.raises(AlreadyFaintedError) as excinfo:
+            battle.take_turn()
+        assert str(excinfo.value) == "A Pokemon has already fainted - time to rest!"
